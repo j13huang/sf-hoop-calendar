d09149ca514b56667e662953bb21753810bedd4f
John Huang <j13huang@gmail.com>
2023-06-09 20:18:08 -0700

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { getLocationData, getSchedules } from "../lib/schedules";
+import { getLocationData } from "../lib/schedules";
 import Calendar from "../components/Calendar";
 import styles from "../styles/Home.module.css";
 
