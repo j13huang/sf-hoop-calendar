7426c933c317b808d71094d26f844bf2f2aec102
John Huang <j13huang@gmail.com>
2023-06-23 10:05:54 -0700

    remove console log

--- pages/api/schedule.ts
+++ pages/api/schedule.ts
@@ -6,10 +6,10 @@ export default async function handler(
   res: NextApiResponse
 ) {
   let location = req.query.location;
-  console.log("start", new Date());
+  //console.log("start", new Date());
   let result = await getSchedule(
     typeof location === "string" ? location : location[0]
   );
-  console.log("end", new Date());
+  //console.log("end", new Date());
   res.status(200).json(result);
 }
